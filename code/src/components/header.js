cfd61daaf615761eac061abb480b219009f7ed11
Mattias Andersson <mattias.andersson@aftonbladet.se>
2018-04-19 00:06:55 +0200

    Styling and added image

--- code/src/components/header.js
+++ code/src/components/header.js
@@ -5,13 +5,16 @@ class Header extends React.Component{
   render() {
     return (
       <div className="header">
+        <div className="header-image-container">
+          <img className="header-image" src= { require('./images/time.png') }  />
+        </div>
         <h1>Welcome to the ReactTimeApp</h1>
         <p>With this app you are able to track the time you spend on the activities of your choice.</p>
-        <ol>
-          <li>Define the activity you want to track using the "Add Activity" field below.</li>
-          <li>Submit the time (minutes) you are dedicating to that activity.</li>
-          <li>And what else???</li>
-        </ol>
+        <div>
+          <p>1. Define the activity you want to track using the "Add Activity" field below.</p>
+          <p>2. Submit the time (minutes) you are dedicating to that activity.</p>
+          <p>3. And what else???</p>
+        </div>
       </div>
     )
   }
