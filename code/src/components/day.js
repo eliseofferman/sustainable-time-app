141bba311995e665e5685d331ba051904a38bebc
elise offerman <eliseofferman@hotmail.com>
2018-04-23 15:36:39 +0200

    took away State is active and fixed button to keep color when page is reloaded

--- code/src/components/day.js
+++ code/src/components/day.js
@@ -2,24 +2,15 @@ import React from "react"
 import "./day.css"
 
 class Day extends React.Component{
-  state = {
-    isActive: false
-  }
 
   handleClick = () => {
-
-    this.setState({
-      isActive: !this.state.isActive
-    })
    const time = prompt("add your time")
    this.props.callbackTimeTable(time, this.props.indexcolumn, this.props.indexrow)
   }
 
   render() {
-    console.log(this.props.index)
-
     return (
-      <div className={this.state.isActive ? "active" : "not-active"}
+      <div className={this.props.time ? "active" : "not-active"}
         onClick={this.handleClick}>
         {this.props.time}
       </div>
