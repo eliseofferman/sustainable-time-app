9f433553ed64fb1e1a7affa55372c0b3e44c3923
elise offerman <eliseofferman@hotmail.com>
2018-04-24 16:40:30 +0200

    add total time to activity

--- code/src/components/tablerow.js
+++ code/src/components/tablerow.js
@@ -3,6 +3,21 @@ import Day from "./day"
 import "./tablerow.css"
 
 class TableRow extends React.Component {
+ renderTotalActivityTime = () => {
+   const numbers = this.props.days
+   let sum = 0
+   for (let i = 0; i < numbers.length; i++) {
+     sum += numbers[i]
+   }
+
+   if (sum < 60) {
+     return (`${sum} min`)
+   } else {
+     const hours = Math.floor(sum / 60)
+     sum = sum - (hours * 60)
+     return (`${hours}h, ${sum}min`)
+   }
+ }
 
   deleteRow = () => {
     this.props.callbackDelete(this.props.index)
@@ -14,14 +29,12 @@ class TableRow extends React.Component {
       <div className="tablerow">
         <h1>{this.props.activityname}</h1>
         <div className="week">
-          {this.props.days.map( (day, index) =>
-
-            // console.log("day", day)
-              <Day time={day} callbackTimeTable={this.props.callbackTime} indexcolumn={index} indexrow={this.props.index}/>
-          )}
+          {this.props.days.map(( day, index) =>
+            <Day time={day} callbackTimeTable={this.props.callbackTime} indexcolumn={index} indexrow={this.props.index} />) }
         </div>
-        <div className="removeButton"onClick={this.deleteRow}>
+        <div className="removeButton" onClick={this.deleteRow}>
           <button>Remove Activity</button>
+          {this.renderTotalActivityTime()}
         </div>
 
       </div>
@@ -31,7 +44,3 @@ class TableRow extends React.Component {
 }
 
 export default TableRow
-
-
-{/*
-<h2>{this.props.days}</h2> */}
