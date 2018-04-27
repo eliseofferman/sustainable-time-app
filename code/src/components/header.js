296e788196f78e87b78f32037bdcf4bde79558bc
Davidlicona <davidlicona@users.noreply.github.com>
2018-04-27 09:15:19 +0200

    Header
    Text

--- code/src/components/header.js
+++ code/src/components/header.js
@@ -8,12 +8,12 @@ class Header extends React.Component{
         <div className="header-image-container">
           <img className="header-image" src= { require('./images/time.jpeg') }  />
         </div>
-        <h2>This app allows you to track the time spent on any activity of your choice.</h2>
+        <h2>The Sustainable Time App is a habit and time tracker. </h2>
         <div className="instructions">
           <p>1. Type and submit your activity with the form below.</p>
           <p>2. Choose a day inside of your activity by clicking on it.</p>
           <p>3. Input the time in minutes.</p>
-          <p>4. You can update that day by clicking again on it.</p>
+          <p>4. You can always update a box! :D</p>
         </div>
       </div>
     )
