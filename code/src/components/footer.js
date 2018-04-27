caf78a0e722e405ed124ae14515071d5c1e82ff5
Davidlicona <davidlicona@users.noreply.github.com>
2018-04-27 13:54:56 +0200

    Footer
    Text

--- code/src/components/footer.js
+++ code/src/components/footer.js
@@ -6,6 +6,8 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
+      <p>Designed and built by: David Licona, Elise Offerman, Erika Adamsson & Mattias Andersson @Technigo & Kivra, 2018.</p>
+
       </div>
     )
   }
